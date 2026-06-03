fc26368961e9eca5e42367ebe25058adb3d24cf8
Daisuke Suzuki <bboydaisuke@gmail.com>
2026-06-03 22:37:06 +0900

    Update Chapter01GameModeBase.cpp

--- Source/ue5cpp/Chapter01GameModeBase.cpp
+++ Source/ue5cpp/Chapter01GameModeBase.cpp
@@ -7,4 +7,14 @@ void AChapter01GameModeBase::BeginPlay()
 {
 	Super::BeginPlay();
 	UE_LOG(LogTemp, Warning, TEXT("Some Warning Message"));
+	
+	int i = 5;
+	float f = 3.7f;
+	FString s = "string";
+	UE_LOG(LogTemp, Warning, TEXT("Text, %d %f %s"), i, f, *s);
+	
+	FString name = "Tim";
+	int32 mana = 450;
+	FString str = FString::Printf(TEXT("Name = %s, Mana = %d"), *name, mana);
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *str);
 }
