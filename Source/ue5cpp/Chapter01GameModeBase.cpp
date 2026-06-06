27fb7e68ef4033f617f3edb2e230593b0316e582
Daisuke Suzuki <bboydaisuke@gmail.com>
2026-06-06 20:30:41 +0900

    Update Chapter01GameModeBase.cpp

--- Source/ue5cpp/Chapter01GameModeBase.cpp
+++ Source/ue5cpp/Chapter01GameModeBase.cpp
@@ -17,4 +17,12 @@ void AChapter01GameModeBase::BeginPlay()
 	int32 mana = 450;
 	FString str = FString::Printf(TEXT("Name = %s, Mana = %d"), *name, mana);
 	UE_LOG(LogTemp, Warning, TEXT("%s"), *str);
+	
+	FString name2 = "Tim";
+	int32 mana2 = 450;
+	TArray<FStringFormatArg> args;
+	args.Add(FStringFormatArg(name2));
+	args.Add(FStringFormatArg(mana2));
+	FString string = FString::Format(TEXT("Name = {0} Mana = {1}"), args);
+	UE_LOG(LogTemp, Warning, TEXT("Your string: %s"), *string);
 }
